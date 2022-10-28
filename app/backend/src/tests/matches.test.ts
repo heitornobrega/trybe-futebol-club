1c80f8aa452520fa65a5c5e223e8e8b3711653a7
heitornobrega <nobheitor@gmail.com>
2022-10-28 17:07:03 -0300

    testes de integraçao rota POST /match

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -32,6 +32,13 @@ describe('Rotas', () => {
       expect((await httpResponse).status).to.equal(200)
     })
   })
+
+  describe('Rota GET /matches?inProgress=false', () => {
+    it('deve retornar status 200', async () => {
+      const httpResponse = chai.request(app).get('/matches?inProgress=false');
+      expect((await httpResponse).status).to.equal(200)
+    })
+  })
   
   describe('Rota POST /matches', () => {
     it('deve retornar status 201', async () => {
@@ -54,6 +61,39 @@ describe('Rotas', () => {
         })
         .set('Authorization', token.token);
       expect((await httpResponse2).status).to.equal(201)
-    }) })
+    })
+  })
+  
+
+  describe('Rota POST /matches', () => {
+    it('deve retornar status 422', async () => {
+      const httpResponse =  chai
+      .request(app)
+      .post("/login")
+      .send({ 
+        email: "user@user.com",
+        password: "secret_user"
+      });
+    expect((await httpResponse).status).to.equal(200)
+     const token = (await httpResponse).body
+      const httpResponse2 = chai.request(app)
+        .post('/matches')
+        .send({
+          homeTeam: 16,
+          awayTeam: 16, 
+          homeTeamGoals: 2,
+          awayTeamGoals: 2,
+        })
+        .set('Authorization', token.token);
+      expect((await httpResponse2).status).to.equal(422)
+    })
+  })
+
+  describe('Rota GET /matches/:id/finish', () => {
+    it('deve retornar status 200', async () => {
+      const httpResponse = chai.request(app).patch('/matches/41/finish');
+      expect((await httpResponse).status).to.equal(200)
+    })
+  })
 
 });
